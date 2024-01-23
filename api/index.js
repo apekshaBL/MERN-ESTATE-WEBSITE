616ef33211dd264a169070d607458e409493a523
Apeksha Bhausaheb LOkhande <apekshalokhande8600@gmail.com>
2024-01-23 21:29:00 +0530

    user.js file is updated

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from  "dotenv";
+import userRouter from "./routes/user.route.js";
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -10,7 +11,11 @@ mongoose.connect(process.env.MONGO).then(()=>{
 });
 
 const app=express();
+
 app.listen(3000,()=>{
     console.log("server is running on port 3000!!!!!!");
 });
+
+app.use("/api/user",userRouter);
+
 
