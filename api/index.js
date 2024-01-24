25796c7e0da4755126f24d84f4159e020a3acbaf
Apeksha Bhausaheb LOkhande <apekshalokhande8600@gmail.com>
2024-01-24 20:30:58 +0530

    create a test api routes

--- api/index.js
+++ api/index.js
@@ -16,6 +16,9 @@ app.listen(3000,()=>{
     console.log("server is running on port 3000!!!!!!");
 });
 
-app.use("/api/user",userRouter);
+app.use("/api/user",userRouter); 
+
+   
+
 
 
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,6 @@
 import express from 'express';
+import {test} from "../controller/user.controller.js";
 const router=express.Router();
 
-router.get("/test",(req,res)=>{
-    res.json({
-        message:"Hello world",
-    });
-});
+router.get("/test",test)
 export default router;
