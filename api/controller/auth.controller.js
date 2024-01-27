21e09dadf23aafaf683e25dcf5c911b4193cf38f
Apeksha Bhausaheb LOkhande <apekshalokhande8600@gmail.com>
2024-01-27 15:38:25 +0530

    fuctionality is updated

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,16 +1,17 @@
 import User from '../models/user.model.js';
 import bcryptjs from "bcryptjs";
-import{errorHandler} from '../utils/error.js';
+//import{errorHandler} from '../utils/error.js';
 
 export const signup=async(req,res,next)=>{
     const {username,email,password}=req.body;
     const hashedPassword=bcryptjs.hashSync(password,10);
-    const newUser=new User ({username,email,password:hashedPassword});
+    const newUser=new User({username,email,password:hashedPassword});
     try{
         await newUser.save();
         res.status(201).json('user created successfully');
     }catch(error){
-        next(error);
+       // next(error);
+       console.log(error);
     }
-   
+    
 };
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const userSchema=new mongoose.Schema({
     username:{
         type:String,
-        required:true,
+       // required:true,
         unique:true
     },
     email:{
