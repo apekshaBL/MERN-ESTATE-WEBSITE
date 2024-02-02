d4fe485e4f3f793488ed8dd17188e5c325e6d611
Apeksha Bhausaheb LOkhande <apekshalokhande8600@gmail.com>
2024-02-02 21:29:58 +0530

    index.js file is updated

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ const app=express();
 app.use(express.json());
 
 app.listen(3000,()=>{
-    console.log("server is running on port 3000!!!!!!");
+    console.log("server is running on port 3000");
 });
 
 app.use("/api/user",userRouter); 
