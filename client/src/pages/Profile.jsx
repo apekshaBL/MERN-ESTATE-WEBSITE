ae46c52e5da0856eaef81f93960f171b3da24ebf
Apeksha Bhausaheb LOkhande <apekshalokhande8600@gmail.com>
2024-03-02 14:42:29 +0530

    complete signOut functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,8 @@
- import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useRef ,useState,useEffect} from 'react';
 import {getDownloadURL, getStorage,ref,uploadBytesResumable} from 'firebase/storage';
 import {app} from '../firebase.js';
-import { updateUserStart,updateUserSuccess,updateUserFailure, deleteUserFailure, deleteUserSuccess,deleteUserStart } from '../redux/user/userSlice.js';
+import { updateUserStart,updateUserSuccess,updateUserFailure, deleteUserFailure, deleteUserSuccess,deleteUserStart,signInFailure,signInStart,signInSuccess, signOutUserFailure,signOutUserSuccess,signOutUserStart } from '../redux/user/userSlice.js';
 import { deleteUser } from '../../../api/controller/user.controller.js';
 
 
@@ -100,9 +100,23 @@ export default function Profile() {
 
 
 
+    const handleSignOut=async()=>{
+      try{
+        dispatch(signOutUserStart());
+        const res=await fetch('api/auth/signout');
+        const data=await res.json();
+        if(data.success===false){
+          dispatch(signOutUserFailure(data.message));
+          return;
+        }
+        dispatch(signOutUserSuccess(data));
+      } catch(error){
+        dispatch(signOutUserFailure(error.message));
+      }
+    }
 
 
-
+    
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -147,7 +161,7 @@ export default function Profile() {
       </form> 
       <div className='flex justify-between mt-5'>
         <span onClick={handleDeleteUser}   className='text-red-700 cursor-pointer'>Delete Account</span>
-        <span className='text-red-700 cursor-pointer'>Sign out</span>
+        <span onClick={handleSignOut}     className='text-red-700 cursor-pointer'>Sign out</span>
       </div>
       <p className='text-red-700 mt-5'>{error ? error :''}</p>
       <p className='text-green-700 mt-5'>{updateSuccess ?'User is updated successfully !':''}</p>
