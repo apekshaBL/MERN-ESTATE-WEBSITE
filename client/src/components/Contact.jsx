744dc3d5f023ea7fb9e0580f3474b11884dc0463
Apeksha Bhausaheb LOkhande <apekshalokhande8600@gmail.com>
2024-03-09 08:07:37 +0530

    complete contact landlord functionality

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Contact({ listing }) {
   const [landlord, setLandlord] = useState(null);
   const [message, setMessage] = useState('');
+
   const onChange = (e) => {
     setMessage(e.target.value);
   };
@@ -15,18 +16,24 @@ export default function Contact({ listing }) {
         const data = await res.json();
         setLandlord(data);
       } catch (error) {
-        console.log(error);
+        console.error('Error fetching landlord:', error);
       }
     };
     fetchLandlord();
   }, [listing.userRef]);
+
+  const handleSendMessage = () => {
+    const subject = `Regarding ${listing.name}`;
+    const mailtoLink = `mailto:${landlord.email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(message)}`;
+    window.location.href = mailtoLink;
+  };
+
   return (
     <>
       {landlord && (
         <div className='flex flex-col gap-2'>
           <p>
-            Contact <span className='font-semibold'>{landlord.username}</span>{' '}
-            for{' '}
+            Contact <span className='font-semibold'>{landlord.username}</span> for{' '}
             <span className='font-semibold'>{listing.name.toLowerCase()}</span>
           </p>
           <textarea
@@ -39,12 +46,12 @@ export default function Contact({ listing }) {
             className='w-full border p-3 rounded-lg'
           ></textarea>
 
-          <Link
-          to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
-          className='bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95'
+          <button
+            onClick={handleSendMessage}
+            className='bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95'
           >
-            Send Message          
-          </Link>
+            Send Message
+          </button>
         </div>
       )}
     </>
